f635c3f779d221375ce06c77319f34b571d96bfc
Nicolas Di Filippo <nicolas.difilippo@marconirovereto.it>
2023-05-18 15:00:31 +0200

    SitoFinito(penso)

--- HowToTrainYourDragon/src/app/home/home.component.ts
+++ HowToTrainYourDragon/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DatiServizioService } from '../common/dati-servizio.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,16 @@ import { DatiServizioService } from '../common/dati-servizio.service';
 export class HomeComponent {
 
 
-  constructor(public dati:DatiServizioService){
+  constructor(public service: DatiServizioService,public dati:DatiServizioService, public router: Router){
 
   }
+
+  ngOnInit() {
+    if(this.service.getAbilita() == false){
+      this.router.navigate(['/', 'Login']);
+    }
+  }
+  
 
   Dati(){
     return console.log(this.dati.getDati());
